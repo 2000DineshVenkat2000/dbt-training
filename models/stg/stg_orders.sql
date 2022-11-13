ab0158b157794bb4e1ee82c4a19f1a450dd7874b
Dinesh Kumar Sai M <dineshkumarsai182@gmail.com>
2022-11-13 13:58:29 +0000

    Added seeds for orders

--- models/stg/stg_orders.sql
+++ models/stg/stg_orders.sql
@@ -1,4 +1,5 @@
 SELECT 
+{{ dbt_utils.surrogate_key(['O."OrderID"','P."ProductID"', 'C."CustomerID"']) }} AS SK_ORDERS,
 O."OrderID",
 O."OrderDate",
 O."ShipDate",
@@ -12,12 +13,15 @@ P."ProductID",
 P."Category",
 P."ProductName",
 P."SubCategory",
-{{ markup('"OrderSellingPrice"', '"OrderCostPrice"') }} AS MARKUP
+{{ markup('"OrderSellingPrice"', '"OrderCostPrice"') }} AS MARKUP,
+D.delivery_team
 FROM {{ ref('raw_orders') }} AS O --FROM RAW.GLOBALMART.ORDERS
 LEFT JOIN {{ ref('raw_customers') }} AS C
 ON O."CustomerID" = C."CustomerID"
 LEFT JOIN {{ ref('raw_products') }} AS P
 ON O."ProductID" = P."ProductID"
+LEFT JOIN {{ ref('delivery_team') }} AS D
+ON O."ShipMode" = D.ShipMode
 
 
 -- {{ limit_data_in_dev('"OrderDate"') }}
